1532c497252600ba5f0e3f74aee0ed5aa91da136
HubbleCommand <sasha.poirier@gmail.com>
2020-10-23 10:47:27 +0000

    Removed commented code from AttributeValidator

--- src/ThinkStory/AttributeValidator/TSAttributeValidator.php
+++ src/ThinkStory/AttributeValidator/TSAttributeValidator.php
@@ -3,12 +3,6 @@ namespace Concrete\Package\ThinkStory\AttributeValidator;
 use Concrete\Core\Attribute\Key\CollectionKey;
 class TSAttributeValidator {
     public static function checkCollectionAttributeHandleExists($attributeHandleToCheck){
-        /*$ak = \CollectionAttributeKey::getByHandle($attributeHandleToCheck);
-        if ( is_null($ak) || !is_object($ak) || !intval($ak->getAttributeKeyID()) ){
-            return false;
-        } else {
-            return true;
-        }*/
         $keyslist = CollectionKey::getList('Collection');
         foreach($keyslist AS $key){
             if(strcmp($key->getAttributeKeyHandle(), $attributeHandleToCheck) == 0){
