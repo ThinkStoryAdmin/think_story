f2befce16b87629a038b924b2ca1ee230c4626e0
HubbleCommand <sasha.poirier@gmail.com>
2020-11-19 16:09:37 +0000

    Page filter block removed englobing div

--- blocks/t_s_page_list2/view.php
+++ blocks/t_s_page_list2/view.php
@@ -1,83 +1,71 @@
 <?php defined('C5_EXECUTE') or die(_("Access Denied.")) ?>
 <script>
+    let blockID = '<?php echo $bID?>';
     let urlForThisPage = '<?php echo $thisUrl?>'
     let urlFilterAction = '<?php echo $this->action('filter2') ?>'
     let sendToAnotherPage = '<?= $bPostToAnotherPage?>'
     let sendToAnotherPageID = '<?= $cParentID?>'
     let sendToAnotherPageIDURL = '<?= $cParentIDURL?>'
-    let testHelpFixFilt = '<?= $testHelpFixFilt?>'
-    let ENT = '<?= $entityabc?>'
-    console.log("ENT : " + ENT)
-	console.log("testHelpFixFilt : " + testHelpFixFilt)
     console.log("URL filter action: " + urlFilterAction)
     console.log("URL for this page: " + urlForThisPage)
-    console.log('sendToAnotherPage' + sendToAnotherPage)
-    console.log('sendToAnotherPageID' + sendToAnotherPageID)
-    console.log('sendToAnotherPageIDURL' + sendToAnotherPageIDURL)
+    console.log("URL for this pageJS: " + window.location.href.split('?')[0])
+    console.log('sendToAnotherPage: ' + sendToAnotherPage)
+    console.log('sendToAnotherPageID: ' + sendToAnotherPageID)
+    console.log('sendToAnotherPageIDURL: ' + sendToAnotherPageIDURL)
     console.log("View.php ready!")
     //All other JS code is in the view.js
 </script>
-<div>
-    <div class="cts-theme-tertiary-bar-background">
-        <div class="container">
-            <div class="topic_select_parent cts-block-pl2-main ">
-                    <?php
-                        foreach($topictrees as $topictree){
-                            if(!(is_null($topictree))){
-                                $node = $topictree->getRootTreeNodeObject();
-                                $node->populateChildren();
-                                if (is_object($node)) {                    
-                                    //in the ID of the select below, need to use getTreeId instead of getTreeName, as if tree name has spaces this causes following issues
-                                        //Cannot reset select
-                                        //Select params are not registered
-                                        //Select changes are not registered
+<div class="cts-theme-tertiary-bar-background"id=<?php echo $bID?>>
+    <div class="container">
+        <div class="topic_select_parent cts-block-pl2-main ">
+                <?php
+                    foreach($topictrees as $topictree){
+                        if(!(is_null($topictree))){
+                            $node = $topictree->getRootTreeNodeObject();
+                            $node->populateChildren();
+                            if (is_object($node)) {                    
+                                //in the ID of the select below, need to use getTreeId instead of getTreeName, as if tree name has spaces this causes following issues
+                                    //Cannot reset select
+                                    //Select params are not registered
+                                    //Select changes are not registered
+                                ?>
+                                <select 
+                                    class="pagelist2 ts-pl2-topic-dropdown" 
+                                    style="width:<?php echo intval(1/count($topictrees) * 100)-10?>%;margin-right:5px;" 
+                                    
+                                    id="think-story-drop-down3-<?php echo $bID?>-<?php echo $topictree->getTreeId()?>" 
+                                    data-action="topic-select2">
+                                <?php 
+                                //Add null for first option
+                                ?><option value=-1><?php echo t('All ') . tc('TreeName', $topictree->getTreeName());?><?php
+                
+                                //Add all topics as options
+                                foreach($node->getChildNodes() as $topic){
                                     ?>
-                                    <select 
-                                        class="pagelist2 ts-pl2-topic-dropdown" 
-                                        style="width:<?php echo intval(1/count($topictrees) * 100)-10?>%;margin-right:5px;" 
-                                        
-                                        id="think-story-drop-down3-<?php echo $bID?>-<?php echo $topictree->getTreeId()?>" 
-                                        data-action="topic-select2">
+                                    <option
+                                        value= <?php echo $topic->getTreeNodeID(); ?> 
+                                        <?php if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()){ echo(' selected');}?>
+                                    >
                                     <?php 
-                                    //Add null for first option
-                                    ?><option value=-1><?php echo t('All ') . tc('TreeName', $topictree->getTreeName());?><?php
-                    
-                                    //Add all topics as options
-                                    foreach($node->getChildNodes() as $topic){
+                                        //echo $topic->getTreeNodeName();
+                                        //echo tc('TopicName', '%s', $topic->getTreeNodeName());
+                                        echo tc('TopicName', $topic->getTreeNodeName());
                                         ?>
-                                        <option
-                                            value= <?php echo $topic->getTreeNodeID(); ?> 
-                                            <?php if (isset($selectedTopicID) && $selectedTopicID == $topic->getTreeNodeID()){ echo(' selected');}?>
-                                        >
-                                        <?php 
-                                            //echo $topic->getTreeNodeName();
-                                            //echo tc('TopicName', '%s', $topic->getTreeNodeName());
-                                            echo tc('TopicName', $topic->getTreeNodeName());
-                                            ?>
-                                        </option>
-                                        <?php
-                                    }
-                                    ?>
-                                    </select>
-                        
+                                    </option>
                                     <?php
-                                    if (!isset($selectedTopicID)) {
-                                        $selectedTopicID = null;
-                                    }
+                                }
+                                ?>
+                                </select>
+                    
+                                <?php
+                                if (!isset($selectedTopicID)) {
+                                    $selectedTopicID = null;
                                 }
                             }
                         }
-                    ?>
-                    <i class="fa fa-refresh cts-theme-icons" aria-hidden="true" style="padding-right:5px;cursor:pointer;"></i><a id="tsreload" style="cursor:pointer;" class="cts-theme-tertiary-bar"><?= t("Reset") ?></a>
-            </div>
+                    }
+                ?>
+                <i class="fa fa-refresh cts-theme-icons" aria-hidden="true" style="padding-right:5px;cursor:pointer;"></i><a id="tsreload" style="cursor:pointer;" class="cts-theme-tertiary-bar"><?= t("Reset") ?></a>
         </div>
     </div>
-    <!-- <div class='container'>
-        loading icon css taken from https://loading.io/css/ 
-        <div id='loader' style='loader'>
-            <div class="ts-pl2-lds-ring"><div></div></div>
-            </div>
-        <div id="tspages" class="ts-pl2-grid-container">
-        </div>
-    </div> -->
 </div>
