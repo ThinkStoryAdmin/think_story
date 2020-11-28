e5d6b714efe008298c3c62f268850828902d784e
HubbleCommand <sasha.poirier@gmail.com>
2020-11-28 21:58:06 +0000

    Uninstall page update

--- elements/dashboard/uninstall.php
+++ elements/dashboard/uninstall.php
@@ -1,5 +1,5 @@
 <?php   defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<!-- TODO remove this document? It doesn't seem to serve a purpose anymore -->
 <div class="alert alert-danger">
-    <input type="checkbox" name="removeTopics" value="1" /> Removes topics (doesn't work yet).
-    <input type="checkbox" name="removeTables" value="1" /> Removes tables?.
+    <input type="checkbox" name="removeTables" value="1" /> Removes tables?
 </div>
